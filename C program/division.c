1bc6d9c3e3e1dd7b02b8d0c875c46fce3ae2fca8
vishusingh1 <93415878+vishusingh1@users.noreply.github.com>
2021-11-06 18:55:41 +0530

    Add files via upload

---
+++ C program/division.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+   int main(){
+   	
+   	int a;
+   	int b;
+   	int c;
+   	
+   	printf("By Vishal\n");
+   	printf("Enter first no:");
+   	scanf("%d",&a);
+   	printf("Enter second no:");
+   	scanf("%d",&b);
+   	c = a/b;
+   	printf("Sum of two numbers = %d",c);
+   	return 0;
+   }
