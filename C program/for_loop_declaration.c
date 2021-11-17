1111d55fcf2144e50bb8419d9dd755ff57c7b04c
vishusingh1 <93415878+vishusingh1@users.noreply.github.com>
2021-11-17 19:59:06 +0530

    Add files via upload

---
+++ C program/for_loop_declaration.c
@@ -0,0 +1,9 @@
+#include <stdio.h>  
+int main()  
+{  
+    int a,b,c;  
+    for(a=0,b=12,c=23;a<2;a++)  
+    {  
+        printf("%d ",a+b+c);  
+    }  
+}
---
+++ C program/logical_error.c
@@ -0,0 +1,13 @@
+#include <stdio.h>  
+	int main()  
+	{  
+	   int sum=0; // variable initialization  
+	   int k=1;  
+	   for(int i=1;i<=10;i++); 
+   {  
+       sum=sum+k;  
+       k++;  
+	   }  
+	printf("The  value of sum is %d", sum);  
+	    return 0;  
+	}
