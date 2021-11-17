6e08432bebf72d6ce1d4edf3e401ba2fe615e137
vishusingh1 <93415878+vishusingh1@users.noreply.github.com>
2021-11-17 06:20:39 +0530

    Add files via upload

---
+++ C program/data_type_character.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+   char  ch1;
+   char  ch2;
+   char  ch3;
+   char  ch4;
+   
+   ch1 = 'a';      
+   ch2 = '1';
+   ch3 = '$';
+   ch4 = '+';  
+
+   printf( "ch1: %c\n", ch1);
+   printf( "ch2: %c\n", ch2);
+   printf( "ch3: %c\n", ch3);
+   printf( "ch4: %c\n", ch4);
+}
+
