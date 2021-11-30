a5567461b8d9bf7401f3bcfd7548e20199a27ba4
vishusingh23 <93415878+vishusingh1@users.noreply.github.com>
2021-11-30 21:58:14 +0530

    Add files via upload

---
+++ C program/multipleoutputs.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+   // using printf() for multiple outputs 
+   int day = 20;
+   int month = 11;
+   int year = 2021;
+      printf("The date is: %d-%d-%d", day, month, year);
+   return 0;
+}
