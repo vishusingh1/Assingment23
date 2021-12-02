c8b5da163cf709fe532419d00afdd8ef5d5dd9b0
vishusingh23 <93415878+vishusingh1@users.noreply.github.com>
2021-12-02 16:39:14 +0530

    Add files via upload

---
+++ C program/new_tab_and_new_line.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+   char  ch1;
+   char  ch2;
+   char  ch3;
+   char  ch4;
+   
+   ch1 = '\t';      
+   ch2 = '\n';
+
+   printf( "Test for tabspace %c and a newline %c will start here", ch1, ch2);
+}
